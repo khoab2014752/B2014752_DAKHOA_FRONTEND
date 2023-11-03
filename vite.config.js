be40e6d7e524298d052a83cbd777c5dcfa318d49
unknown <anhkhoadoan26022002@gmail.com>
2023-11-03 18:33:49 +0700

    Commit all

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { Server } from 'node:http'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -16,5 +15,11 @@ export default defineConfig({
   },
   server: {
     port: 3001,
-  }
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000/",
+        changeOrigin: true,
+      },
+    }
+  },
 });
